872d638e2d7f2cefdb2366d2052d0b6bfb7f8c4d
sumit <sumitchauhancoe@gmail.com>
2017-10-15 01:15:54 +0530

    Merge branch 'master' into masterGithub
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent} from '../layout/header/header.component';
+import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping/shopping-list-edit/shopping-list-edit.component';
+import { RecipesComponent } from './recipe/recipes/recipes.component';
+import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
+@NgModule({
+  declarations: [
+    AppComponent, HeaderComponent, RecipeListComponent, RecipeItemComponent, ShoppingListComponent, ShoppingListEditComponent, RecipesComponent, RecipeDetailComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
---
+++ src/layout/header/header.component.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by sukumar on 14-10-2017.
+ */
+import {Component} from '@angular/core';
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+
+})
+
+export class HeaderComponent {
+  title:'Recipe Book'
+}
